97a3542fd270730cc0ad6ff8ee67ab4c152568a0
aldihidayat35 <aldihidayat1004@gmail.com>
2026-07-14 00:27:12 +0700

    commit

---
+++ resources/views/layout/partials/_header.blade.php
@@ -0,0 +1,25 @@
+
+<!--begin::Header-->
+<div id="kt_app_header" class="app-header " 
+     
+     >
+                        <!--begin::Header container-->
+            <div class="app-container  container-fluid d-flex align-items-stretch flex-stack " id="kt_app_header_container">
+                <!--begin::Sidebar toggle-->
+<div class="d-flex align-items-center d-block d-lg-none ms-n3" title="Show sidebar menu">
+	<div class="btn btn-icon btn-active-color-primary w-35px h-35px me-2" id="kt_app_sidebar_mobile_toggle">
+		<i class="ki-outline ki-abstract-14 fs-2"></i>	</div>
+	<!--begin::Logo image-->
+    <a href="/metronic8/demo38/?page=index">
+		<img alt="Logo" src="/metronic8/demo38/assets/media/logos/demo38-small.svg" class="h-30px"/>
+    </a>
+    <!--end::Logo image-->
+</div>
+<!--end::Sidebar toggle-->
+
+@include('layout/partials/header/_navbar')
+            </div>
+            <!--end::Header container-->
+            </div>
+<!--end::Header-->
+
---
+++ resources/views/layout/partials/sidebar/_navs.blade.php
@@ -0,0 +1,21 @@
+
+<!--begin::Navs-->
+<div class="app-sidebar-navs flex-column-fluid py-6" id="kt_app_sidebar_navs">    
+	<div 
+		id="kt_app_sidebar_navs_wrappers" 
+		class="app-sidebar-wrapper hover-scroll-y my-2"        
+        data-kt-scroll="true"
+        data-kt-scroll-activate="true"
+        data-kt-scroll-height="auto"     
+        data-kt-scroll-dependencies="#kt_app_sidebar_header"
+        data-kt-scroll-wrappers="#kt_app_sidebar_navs" 
+        data-kt-scroll-offset="5px">
+		
+@include('layout/partials/sidebar/_navs/_teams')
+		
+@include('layout/partials/sidebar/_navs/_menu')
+	</div>
+</div>
+<!--end::Navs-->
+
+
